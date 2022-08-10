44ef14bdfd5d6a0307855ae4a87b0c27a711f839
Touhid Rahman <touhidrahman1987@gmail.com>
2022-08-10 19:07:18 -0400

    Quote state: load quotes on change of filters

--- src/app/features/quote/states/quote-state.service.ts
+++ src/app/features/quote/states/quote-state.service.ts
@@ -7,15 +7,29 @@ import { QuoteService } from '../services/quote.service'
 
 @Injectable()
 export class QuoteStateService extends BasicPageState {
+    tag = new StateSubject<string>('')
+    author = new StateSubject<string>('')
+    limit = new StateSubject<number>(20)
+    sortBy = new StateSubject<'dateAdded' | 'dateModified' | 'author' | 'content'>('dateAdded')
+
     randomQuote = new StateSubject<Quote | null>(null)
+    quoteSearchResponse = new StateSubject<QuoteSearchResponse | null>(null)
 
     constructor(private quoteService: QuoteService) {
         super()
+        this.init()
         this.pollRandomQuote()
     }
 
+    reset(): void {
+        this.tag.reset()
+        this.sortBy.reset()
+        this.author.reset()
+        this.limit.reset()
+        super.reset()
+    }
+
     private pollRandomQuote() {
-        // get a random quote every 10 seconds
         interval(10_000)
             .pipe(
                 switchMap(() => this.quoteService.getRandomQuote()),
@@ -24,6 +38,31 @@ export class QuoteStateService extends BasicPageState {
             .subscribe({
                 next: (quote) => {
                     this.randomQuote.next(quote)
+                },
+            })
+    }
+
+    private init() {
+        // when any of the tag, author, limit, sortBy, page changes, fetch quotes
+        combineLatest({
+            tag: this.tag.value$,
+            author: this.author.value$,
+            limit: this.limit.value$,
+            sortBy: this.sortBy.value$,
+            page: this.page.value$,
+        })
+            .pipe(
+                debounceTime(300), // debounce to prevent multiple requests without reasonable change
+                tap(() => this.loading.next(true)),
+                switchMap(({ tag, author, limit, sortBy, page }) => {
+                    return this.quoteService.listQuotes(author, [tag], [], sortBy, limit, page)
+                }),
+                takeUntil(this.destroyed),
+            )
+            .subscribe({
+                next: (response) => {
+                    this.loading.next(false)
+                    this.quoteSearchResponse.next(response)
                 },
             })
     }
